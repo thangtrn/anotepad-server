1f25a3d924166b197a63bea05f7ebec928af6722
Thang Tran <thangtrn01@gmail.com>
2022-08-20 22:05:07 +0700

    upload code

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ app.use(cors());
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit:'30mb', extended: true}));
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello word.</h1>');
+})
 app.use('/api', routes);
 
 app.listen(PORT, ()=> {
